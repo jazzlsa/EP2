05ffa5a8bd7d427efe07e7d49997c1d314557acf
jesslsa <jessicalsa@al.insper.edu.br>
2022-05-11 00:56:10 -0300

    comecei o corpo do jogo

---
+++ main.py
@@ -0,0 +1,12 @@
+'''continuar='s'
+while continuar=='s':
+  tentativas=20
+  sorteia_pais(dic)
+  while tentativas!=0:
+    
+
+
+  print('Perdeu! O país era {}')
+  continuar=input('Jogar Novamente?s/n ')
+  while continuar!='s' and continuar!='n':
+    continuar=input('Ops! Não entendi. Digite s/n: ')'''
