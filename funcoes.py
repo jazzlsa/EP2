dedbb5a1a1c630e0d268311871c11dea58b125a4
jesslsa <jessicalsa@al.insper.edu.br>
2022-05-11 00:31:57 -0300

    Unifiquei as funções em um arquivo

--- funcoes.py
+++ funcoes.py
@@ -51,3 +51,58 @@ def adiciona_em_ordem(nome, distancia, lista):
             lista_ordenada.append(lista_1)
     
     return lista_ordenada
+
+def sorteia_letra_capital(palavra, lista_restrita):
+
+    especiais = ['.', ',', '-', ';', ' ', '@', '!', '$', '%', '*', '(', ')', '{', '}', '[', ']', ':']
+    saida = ''
+    palavra = palavra.lower()
+    palavra_lista = []
+
+    continua = False
+
+    for letra in palavra:
+
+        if letra not in especiais and letra not in lista_restrita and letra not in palavra_lista:
+            palavra_lista.append(letra)
+            continua = True
+
+    while continua:
+
+        saida = random.choice(palavra_lista)
+        
+       
+        continua = False
+
+    return saida
+    
+def cor_bandeira(bandeira):
+
+    cores_possiveis = []
+    saida = ''
+
+    for cor, quant in bandeira.items():
+        if quant > 0:
+            cores_possiveis.append(cor)
+
+    saida = random.choice(cores_possiveis)
+    
+    if saida == 'outras':
+        saida = random.choice(cores_possiveis)
+
+    return saida
+
+def normaliza(dic):
+
+    saida = {}
+
+    for continente in dic.keys():
+        for pais in dic[continente]:
+            
+            conteudo = dic[continente][pais]
+            
+            saida[pais] = conteudo
+
+            saida[pais]['continente'] = continente
+            
+    return saida
