895641252fed7f7b55142625b804ee39854d9518
jesslsa <jessicalsa@al.insper.edu.br>
2022-05-12 16:16:33 -0300

    coloquei o país escolhido em minusculo

--- main.py
+++ main.py
@@ -11,23 +11,29 @@ ganhou = False #inicia ganhou
 desistiu = False #inicia desistiu
 pais_sorteado = NULL #inicia o pais sorteado
 dados_normalizados = funcoes.normaliza(DADOS) # Dicionario com os países (como chave) e seus respectivos dados
-distancias = []
+distancias = [] #inicializando as distancias das tentativas
 
-for pais in dados_normalizados.keys(): # Colocando os países na lista
+# Colocando os países na lista
+for pais in dados_normalizados.keys():
   lista_paises.append(pais)
 
-
 while continuar == 's': # laço para garantir que ele continue jogando até dizer que não
-
-  quantidade_tentativas = 20 #inicia quantidade de tentativas
-  pais_sorteado = funcoes.cria_dicionario_pais_sorteado(funcoes.sorteia_pais(dados_normalizados),dados_normalizados) #define qual o país para se ganhar
-  display.display_boas_vindas() #mostra as boas vindas do jogo
-  display.display_menu() #mostra as boas vindas do jogo
+  #inicia quantidade de tentativas
+  quantidade_tentativas = 20
+  #define qual o país para se ganhar
+  pais_sorteado = funcoes.cria_dicionario_pais_sorteado(funcoes.sorteia_pais(dados_normalizados),dados_normalizados)
+  #mostra as boas vindas do jogo
+  display.display_boas_vindas()
+  #mostra o menu
+  display.display_menu()
 
   #continua não tiver desistido ou ganhado ou tiver tentativas
   while quantidade_tentativas != 0 or ganhou == False or desistiu == False:
     #pega o que ele for digitar
     entrada = input('Qual é o país sorteado? ')
+    #dar .lower() no pais esolhido pelo jogador 
+    
+    #pula linha
     print()
 
     if funcoes.esta_na_lista(entrada, dados_normalizados)==True and (entrada != pais_sorteado['nome']):
@@ -37,15 +43,15 @@ while continuar == 's': # laço para garantir que ele continue jogando até dize
       distancia_tentativa = funcoes.haversine(pais_sorteado['latitude'], pais_sorteado['longitude'], dados_normalizados[entrada]['geo']['latitude'], dados_normalizados[entrada]['geo']['longitude'])
       #adiciona a distância do país informado a lista de tentativas
       distancias.append(entrada+': '+distancia_tentativa+'km')
+      #mostra todas as distancias
       display.display_distancias(distancias)
+      #mostra o restante das tentativas
       display.display_tentativas_restantes(quantidade_tentativas)
     else:
       if(entrada=='menu'):
         display.display_menu()
       if(entrada=='dica'):
         display.display_mercado_dicas()
-        ############
-        #Precisa terminar
       if(entrada=='desisto'):
         display.display_confirma_desisto()
         entrada = input()
@@ -58,7 +64,8 @@ while continuar == 's': # laço para garantir que ele continue jogando até dize
           display.display_nao_desisto()
       if(entrada=='inventario'):
         print('Não sei o que é isso')
-      print('qualquer coisa')
+      if(entrada not in ['menu','dica','desisto']):
+        print('Eita! O valor digitado não é um país. Lembre-se de não digitar acentos e letras maiúsculas')
 
 # 
 # 
