24ec71794af2e72c2801b3d99ef57bc115b5bf95
Verônica Lima <veronicalrl@al.insper.edu.br>
2022-05-15 13:53:02 -0300

    colocando a raposinha no inicio do jogo

--- tentando_usar_imagem.py
+++ tentando_usar_imagem.py
@@ -7,6 +7,12 @@ raposinha = []
 for linha in linhas:
     raposinha.append(linha)
 for li in raposinha:
-    print('\033[1;31m'+str(li)+'\033[m', end='')
+    print('\033[1;35m'+str(li)+'\033[m', end='')
 
-
+'''
+print('\033[1;30;45m  ✿ ❀ ✿ ❀ ✿ ❀ ✿ ❀ ✿ ❀ ✿ ❀ ✿ ❀ ✿ ❀   \033[m')
+    print('\033[1;30;45m ☺                                ☺ \033[m')
+    print('\033[1;30;45m ☺   Bem-vindo ao Insper Países   ☺ \033[m')
+    print('\033[1;30;45m ☺                                ☺ \033[m')
+    print('\033[1;30;45m  ✿ ❀ ✿ Design de Software✿ ❀ ✿ ❀   \033[m')
+'''
