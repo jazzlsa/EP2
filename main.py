2f8345f967d8a67529ce826029dee8113da300db
jesslsa <jessicalsa@al.insper.edu.br>
2022-05-15 13:29:12 -0300

    corrigindo erros no main

--- main.py
+++ main.py
@@ -87,10 +87,12 @@ while continuar == 's':
           #mostra mensagem de desistencia
           display.display_sim_desisto()
           desistiu=True
+          entrada='desisto'
         #negação de desistencia
         if(entrada=='n'):
           #mostra mensagem de negação de desistencia
           display.display_nao_desisto()
+          entrada='desisto'
 
       # mostra inventário
       if(entrada=='inventario'):
@@ -153,7 +155,7 @@ while continuar == 's':
           display.display_tentativas_restantes(quantidade_tentativas)
         
       #verifica se não é uma das opções. Se não for mostra msg de informação errada
-      if entrada not in ['menu','dica','desisto','s','n','inventario'] and entrada != pais_sorteado['nome']:
+      if entrada not in ['menu','dica','desisto','inventario'] and entrada != pais_sorteado['nome']:
         print('Eita! O valor digitado não é um país. Lembre-se de não digitar acentos.')
       #verifica se o pais digitado é o correto
       if(entrada==pais_sorteado['nome']):
@@ -176,5 +178,6 @@ while continuar == 's':
     if(entrada!='s' and entrada!='n'):
       display.digite_s_ou_n()
   continuar=entrada
+  entrada='desisto'
 #mostra mensagem final
 display.produtivo()
